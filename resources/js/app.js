123d978279fdb9ea10845ec84aba01084f15c9b7
Lxasca <aestdata@gmail.com>
2026-02-25 10:03:44 +0700

    1 - Initialisation du projet

---
+++ resources/js/app.js
@@ -0,0 +1,18 @@
+import './bootstrap';
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "./pages/HomePage.vue";
+
+const routes = [
+    { path: "/", component: HomePage },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
---
+++ resources/js/pages/HomePage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>Pipeline</h1>
+
+        <section id="step-1">
+            <h2>Etape 1</h2>
+            <p>
+                Scraper les villes de France sur linternaute.com
+            </p>
+
+            <div>
+                <form @submit="step1()">
+                    <label for="from">Numéro de la première ville à scrapper</label>
+                    <input type="number" name="from">
+
+                    <label for="from">Nombre de villes à scrapper</label>
+                    <input type="number" name="from">
+
+                    <button>Lancer</button>
+                </form>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HomePage",
+};
+</script>
